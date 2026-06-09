42c5fce7553b5767be58112c07db1977966f755b
huyndse161632 <huyndse161632@fpt.edu.vn>
2026-06-09 07:14:21 +0700

    fix: change ts-expect-error to ts-ignore

--- components/posts/CKEditorWrapper.tsx
+++ components/posts/CKEditorWrapper.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-// @ts-expect-error Types missing in older versions
+// @ts-ignore
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { uploadApi } from '@/utils/api';
 
